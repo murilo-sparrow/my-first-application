ca6a512c85ebe131f6211490a41b35c2afcbc29c
Murilo <murilo.batista.barros@gmail.com>
2020-03-11 19:46:03 -0300

    Fiz minha própria função

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 alert("Esse é o meu primeiro alert.")
+
 console.info("Esse é o meu console do navegador e estou gostando muito.")
+
+function mostrarMensagem() {
+    alert("Essa é uma mensagem dentro da função.")
+}
